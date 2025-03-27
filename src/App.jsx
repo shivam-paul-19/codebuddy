8d4f0d38adf1d960f7faa2e51c2e752216c62a6d
Shivam Paul <shivampaul2319@gmail.com>
2025-03-27 19:35:53 +0530

    basic functioning and gemini API

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getQuestion = async (event) => {
+    event.preventDefault();
+    let url = event.target[0].value
+    let lang = event.target[1].value
+    let quest = url.split('/')
+    if(quest[2] != "leetcode.com") {
+      console.log("sorry")
+      return
+    }
+    console.log("solving your question.....wait")
+    let res = await getResponse(url, lang)
+    quest = quest[4].replaceAll('-', ' ')
+    console.log(res)
+  }
+
+  const getResponse = async (prompt, lang) => {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    prompt = `give answer of this question in ${lang}: ${prompt}
+              the structure should be like this:
+              - question explanation (in plain text, not code)
+              - code in ${lang} (use subtle comments)
+              - dry run
+              - time and space complexity`
+    const result = await model.generateContent(prompt);
+    let response = result.response.text();
+    console.log(response)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Code Buddy</h1>
+    <h2>Your one stop solution for DSA problems</h2>
+    <form action="" onSubmit={getQuestion}>
+    <label htmlFor="link">Enter the link</label>
+    <input type="text" />
+    <select name="" id="">
+      <option value="java">Java</option>
+      <option value="C++">C++</option>
+      <option value="Python">Python</option>
+      <option value="C">C</option>
+      <option value="javascript">JavaScript</option>
+      <option value="typescript">TypeScript</option>
+      <option value="PHP">PHP</option>
+      <option value="C#">C#</option>
+      <option value="Dart">Dart</option>
+      <option value="Kotlin">Kotlin</option>
+    </select>
+    <button type='submit'>Enter</button>
+    </form>
     </>
   )
 }
